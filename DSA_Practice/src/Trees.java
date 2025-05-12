1f75fb80920fbb7b8f9b27b8d4c1e7e4cf94c913
TarunM1012 <34863641+TarunM1012@users.noreply.github.com>
2025-05-12 09:28:10 -0400

    trees

--- DSA_Practice/src/Trees.java
+++ DSA_Practice/src/Trees.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Trees {
     static class Node {
         int data;
@@ -21,16 +22,82 @@ public class Trees {
             newNode.left = buildTree(nodes);
             newNode.right = buildTree(nodes);
             return newNode;
+        }
+        public static void preorder(Node root){
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+        public static void inorder(Node root){
+            if (root == null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
 
+        }
+        public static void postorder(Node root){
+            if(root == null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+        }
+        public static void levelOrder(Node root){
+            if(root == null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }
+                    else {
+                        q.add(null);
+                    }
+
+                }
+                else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+
+                    }
+                    if(currNode.right!= null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
 
         }
 
+
     }
+
     public static void main(String[] args) {
         int nodes [] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
-
+      //  System.out.println(root.data);
+        tree.preorder(root);
+        System.out.println(" ");
+        tree.inorder(root);
+        System.out.println(" ");
+        tree.postorder(root);
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        tree.levelOrder(root);
     }
 }
