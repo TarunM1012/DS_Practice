69a3500234cd0cab758d04f557cd5a9850413f35
TarunM1012 <34863641+TarunM1012@users.noreply.github.com>
2025-05-30 12:11:06 -0400

    f

---
+++ DSA_Practice/src/prioQ.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+public class prioQ {
+    public static void main (String args[]){
+        //PriorityQueue <Integer> pq = new PriorityQueue<> ();//ascending value
+        PriorityQueue <Integer> pq = new PriorityQueue<> (Comparator.reverseOrder());//ascending value
+
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+
+
+
+    }
+}
